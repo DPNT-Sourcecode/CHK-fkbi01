d9447d7546a1e5557155d1d5ede52ab94af9d575
sbx_user1051 <sbx_user1051@169.254.109.245>
2022-06-16 21:02:17 +0000

    Thu Jun 16 21:02:17 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+
+    data = input('Enter items with space: ')
+    user_list = data.split()
+    data_upper = [name.upper() for name in user_list]
+    return 0
+    # print('items: ', data_upper)
     
+    
+    
+    
+
+    
+
