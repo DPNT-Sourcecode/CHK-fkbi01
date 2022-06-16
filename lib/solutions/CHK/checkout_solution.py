04b818efbec910e781d44a308d7800dc996cea0e
sbx_user1051 <sbx_user1051@169.254.124.197>
2022-06-16 22:27:33 +0000

    Thu Jun 16 22:27:33 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -15,19 +15,25 @@ def checkout(skus):
         # print(char)
         # if dic.contain('A', 'B', 'C', 'D'):
         if char == 'A':
-            if dic[char] == 3:
-                total = total + 130
+            if dic[char] % 3 == 0:
+                total = total + ((dic[char] // 3)*130)
+            elif dic[char] > 3:
+                total = total + 130 + (50*(dic[char]-3))
                 # print(total)
             else:
                 total = total + (50*dic[char])
-                # print(total)
+                print(total)
         if char == 'B':
-            if dic[char] == 2:
-                total = total + 45
+            mod = dic[char] // 2
+            if dic[char] % 2 == 0:
+                total = total + ((dic[char] // 2)*45)
+            elif dic[char] > 2:
+                total = total + (45*mod) + (30*(dic[char]-(mod*2)))
                 # print(total)
             else:
                 total = total + (30*dic[char])
-                # print(total)
+                print(total)
+        
         if char == 'C':
             total = total + (20*dic[char])
 
@@ -40,11 +46,10 @@ def checkout(skus):
         if not char.isalpha() or (char not in "ABCD"):
             return -1
 
-
     return total
     
     
     
-    
 
     
+
